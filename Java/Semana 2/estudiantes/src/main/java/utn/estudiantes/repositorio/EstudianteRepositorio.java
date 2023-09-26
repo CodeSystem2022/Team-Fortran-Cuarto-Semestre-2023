6b215d570d779ad73a9821fff5b8c899d89e4ab8
LucasVizcaino <lucashotmail4@gmail.com>
2023-09-26 17:07:07 -0300

    Solucionando errores Java

--- Java/Semana 2/estudiantes/src/main/java/utn/estudiantes/repositorio/EstudianteRepositorio.java
+++ Java/Semana 2/estudiantes/src/main/java/utn/estudiantes/repositorio/EstudianteRepositorio.java
@@ -3,12 +3,14 @@ package utn.estudiantes.repositorio;
 import utn.estudiantes.modelo.Estudiante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-import org.springgramework.data.domain.Example;
-import org.springgramework.data.domain.Sort;
-import org.springgramework.data.repository.ListCrudRepository;
-import org.springgramework.data.repository.ListPagingAndSortingRepository;
-import org.springgramework.data.repository.NoRepositoryBean;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 
 
 public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer>{
+
+}
