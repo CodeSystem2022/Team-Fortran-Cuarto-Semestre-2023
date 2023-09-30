8d3cbd6ca078e8733e068ae8eac4a831cdc1de46
LucasVizcaino <lucashotmail4@gmail.com>
2023-09-30 15:23:35 -0300

    Creamos un setInterval iterado como un ciclo for #136

--- Node/Semana 5/monohilo.js
+++ Node/Semana 5/monohilo.js
@@ -1,5 +1,7 @@
 console.log('Hola a toda la cohorte 2022')
 
-setInterval(function () {
-    console.log('Sigo activo')
+var i = 0;
+setInterval(function() {
+    console.log(i);
+    i++;
 }, 1000);
