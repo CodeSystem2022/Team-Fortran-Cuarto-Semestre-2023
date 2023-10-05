525b9911002530e4c82611f52d77012d228f922f
MatiasNicolas2013 <nimats2018@gmail.com>
2023-10-06 00:51:51 +0200

    Ejercicio Semana 5-Python -Metodo Split

--- Python/Semana 5/metodo_split.py
+++ Python/Semana 5/metodo_split.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # help(str.split)
 #Segmento de MatiasNicolas
 
@@ -12,7 +12,6 @@ lista_cursos = cursos_separados_coma.split(',', 2)
 print(f'Lista de cursos: {lista_cursos}')
 print(len(lista_cursos))
 
-=======
 help(str.split)
 #Segmento de Gaston Morales
 
@@ -24,4 +23,4 @@ cursos_separados_coma = 'Java,Python,Node,JavaScript,Spring'
 lista_cursos = cursos_separados_coma.split(",",2)
 print(f'Lista de cursos: {lista_cursos}')
 print(len(lista_cursos))
->>>>>>> c3cd11eea1f36870885d889a6ad661766d464d78
+#Codigo terminado
