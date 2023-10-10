5c0c8d6cc8dd804e5775d013932abbe33e07bd89
califerdan <califerdan@gmail.com>
2023-10-10 20:19:44 -0300

    Metodo actualizarTarea #280

--- JavaScript/Proyecto con stack pern (Daniel Guerrero)/src/controllers/tareas.controllers.js
+++ JavaScript/Proyecto con stack pern (Daniel Guerrero)/src/controllers/tareas.controllers.js
@@ -36,8 +36,19 @@ export const crearTarea = async (req, res) => {
     next(error);
   }
 };
-export const actulizarTarea = (req, res) =>
-  res.send("Actualizando la tarea unica");
+
+export const actualizarTarea = async (req, res) => {
+  const { titulo, descripcion } = req.body;
+  const id = req.params.id;
+  const result = await pool.query(
+    "UPDATE tareas SET titulo = $1, descripcion = $2 WHERE id = $3",
+    [titulo, descripcion, id]
+  );
+  if (result.rowCount === 0) {
+    return res.status(404).json({ message: "No existe una tarea con ese id" });
+  }
+  return res.json(result.rows[0]);
+};
 
 export const eliminarTarea = async (req, res) => {
   const resultado = await pool.query("DELETE FROM tareas WHERE id = $1", [
