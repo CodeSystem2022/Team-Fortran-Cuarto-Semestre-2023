d16f88b70493de9e260fea0ccdd59552ae69c4a1
califerdan <califerdan@gmail.com>
2023-10-10 20:17:23 -0300

    Método para obtener tarea -> listarTarea en singular #278

--- JavaScript/Proyecto con stack pern (Daniel Guerrero)/src/controllers/tareas.controllers.js
+++ JavaScript/Proyecto con stack pern (Daniel Guerrero)/src/controllers/tareas.controllers.js
@@ -1,38 +1,41 @@
-import {pool} from '../db.js';
+import { pool } from "../db.js";
 
-export const listarTareas = async(req, res)=> {
+export const listarTareas = async (req, res) => {
+  const resultado = await pool.query("SELECT * FROM tareas");
+  console.log(resultado);
+  return res.json(resultado.rows);
+};
 
-        const resultado = await pool.query('SELECT * FROM tareas');
-        console.log(resultado);
-        return res.json(resultado.rows);
+export const listarTarea = async (req, res) => {
+  const resultado = await pool.query("SELECT * FROM tareas WHERE id = $1", [
+    req.params.id,
+  ]);
+  if (resultado.rowCount === 0) {
+    return res.status(404).json({ message: "La tarea no existe" });
+  }
+  return res.json(resultado.rows[0]);
+};
 
-}
+export const crearTarea = async (req, res) => {
+  const { titulo, descripcion } = req.body;
 
-
-export const listarTarea =(req,res)=>res.send('Obtener tarea unica');
-
-export const crearTarea = async(req,res)=> {
-    const {titulo, descripcion} = req.body;
-
-    
-    try {
-        const result = await pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2) RETURNING *', [titulo, descripcion]);
-        res.json(result.rows[0]);
-        console.log(result.rows[0]);
-    } catch (error) {
-        if(error.code === '23505') {
-            return res.status(409).json({
-                message: 'Ya existe una tarea con ese titulo'
-
-            });
-
-
-        }  
-        console.log(error);
-        next(error);
+  try {
+    const result = await pool.query(
+      "INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2) RETURNING *",
+      [titulo, descripcion]
+    );
+    res.json(result.rows[0]);
+    console.log(result.rows[0]);
+  } catch (error) {
+    if (error.code === "23505") {
+      return res.status(409).json({
+        message: "Ya existe una tarea con ese titulo",
+      });
     }
-
-
-}
-export const actulizarTarea =(req,res)=>res.send('Actualizando la tarea unica');
-export const eliminarTarea =(req,res)=>res.send('Eliminado la tarea unica')
+    console.log(error);
+    next(error);
+  }
+};
+export const actulizarTarea = (req, res) =>
+  res.send("Actualizando la tarea unica");
+export const eliminarTarea = (req, res) => res.send("Eliminado la tarea unica");
