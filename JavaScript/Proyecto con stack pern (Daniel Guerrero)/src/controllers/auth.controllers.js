2e79a41dfee7104db4e5516ea1022f2c0a8fef36
danisa-morales <moralesc.danisa@gmail.com>
2023-10-12 23:13:33 -0300

    7.3 Creación del token -> conectando con el frontend - 7.4 Cookies -> Guardar el token en una cookie

--- JavaScript/Proyecto con stack pern (Daniel Guerrero)/src/controllers/auth.controllers.js
+++ JavaScript/Proyecto con stack pern (Daniel Guerrero)/src/controllers/auth.controllers.js
@@ -1,30 +1,32 @@
 import {pool} from "../db.js"
-import bcrypt from  "bcrypt";
+import bcrypt from "bcrypt";
+import { createAccessToken } from "../libs/jwt.js";
 
-export const signin = (req,res)=>res.send('Ingresando');
+export const signin = (req, res) => res.send('ingresando');
 
-export const signup = async(req,res)=>{
-    const{name, email, password} = req.body;
+export const signup = async (req, res) => {
+    const {name, email, password} = req.body;
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
         console.log(hashedPassword);
 
-        const result = await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3) RETURNING *", [name, email, hashedPassword])
-
-        console.log(result.rows);
+        const result = await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($!, $2, $3) RETURNING", [name, email, hashedPassword])
+       
+        const token = await createAccessToken({id: result.rows[0].id});
+        console.log(result);
+        res.cokkie("token", token, { 
+            httpOnly: true,
+            sameSite: "none",
+            maxAge: 60 * 60 * 24 * 1000,}) // 1 day
         return res.json(result.rows[0]);
-
     } catch (error) {
-        if (error.code === "23505"){
-            return res.status (400).json({message: "El correo ya esta registrado"});
-        }
-        
+        if(error.code === "23505"){
+            return res.status(400).json({message: "El correo ya está registrado"});
+        } 
     }
-
-
 };
 
-export const sigoup = (req,res)=>res.send('cerrando sesion');
+export const signout = (req, res) => res.send('Cerrando sesion');
 
-export const profile = (req,res)=>res.send('perfil de usuario')
+export const profile = (req, res) => res.send('Perfil del usuario');
