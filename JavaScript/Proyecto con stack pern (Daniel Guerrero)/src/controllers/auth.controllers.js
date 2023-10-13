0e4397c85aa2eabdaa3476297eb87202038d2c1b
Facundo Pereyra <111472242+Gise07@users.noreply.github.com>
2023-10-12 21:08:04 -0300

    7.1 Registro - 7.2 Utilización de bcrypt
    7.1 Registro -> Archivo auth.routes.js => signup #311
    
    7.2 Utilización de bcrypt para proteger el password #312

--- JavaScript/Proyecto con stack pern (Daniel Guerrero)/src/controllers/auth.controllers.js
+++ JavaScript/Proyecto con stack pern (Daniel Guerrero)/src/controllers/auth.controllers.js
@@ -1,4 +1,30 @@
+import {pool} from "../db.js"
+import bcrypt from  "bcrypt";
+
 export const signin = (req,res)=>res.send('Ingresando');
-export const signup = (req,res)=>res.send('Registrando');
-export const sigoup = (req,res)=>res.send('cerrando seción');
+
+export const signup = async(req,res)=>{
+    const{name, email, password} = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        console.log(hashedPassword);
+
+        const result = await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3) RETURNING *", [name, email, hashedPassword])
+
+        console.log(result.rows);
+        return res.json(result.rows[0]);
+
+    } catch (error) {
+        if (error.code === "23505"){
+            return res.status (400).json({message: "El correo ya esta registrado"});
+        }
+        
+    }
+
+
+};
+
+export const sigoup = (req,res)=>res.send('cerrando sesion');
+
 export const profile = (req,res)=>res.send('perfil de usuario')
