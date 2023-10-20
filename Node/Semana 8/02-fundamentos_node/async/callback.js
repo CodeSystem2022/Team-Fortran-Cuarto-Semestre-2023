c7eef950e19730d96d64e21231f12edfb28216cb
Paola Del Valle Ruiz <paola.ruiz@segurossura.com.ar>
2023-10-20 19:33:18 -0300

    8.2 Creamos asincronía con el setTimeout

---
+++ Node/Semana 8/02-fundamentos_node/async/callback.js
@@ -0,0 +1,10 @@
+function soyAsincrona(){
+    setTimeout(function(miCallback) {
+        console.log('Hola soy una funcion asincrona');
+    }, 1000);
+    
+}
+console.log('Iniciando el proceso....');
+soyAsincrona(function(){
+    console.log('Terminado el proceso...');
+});
