e64a72f109e88f9f17fe153e164794812573063e
MatiasNicolas2013 <nimats2018@gmail.com>
2023-10-23 00:49:21 +0200

    Ejercicio 8.2 -JAVA

--- Java/Semana 6-7-8/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Semana 6-7-8/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,16 +1,26 @@
 package utn.tienda_libros;
-
+import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroFrom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.EventQueue;
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
-                SpringApplication.run(TiendaLibrosApplication.class, args);
-		ConfigurableApplicationContext contextoSpring = 
+	ConfigurableApplicationContext contextoSpring = 
 		new SpringApplicationBuilder(TiendaLibrosApplication.class)
-	}
+		       .headless(false)
+			   .web(WebApplicationType.NONE)
+			   .run(args);
+
+		//Ejecutamos el codigo para cargar el formulario
+		EventQueue.invokeLater(() ->{ //Metodo Lambda
+			//Obtenemos el objeto from a traves del spring
+			LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
+            libroFrom.setVisible(true);   
+		}); 	   
+		}
 
 }
