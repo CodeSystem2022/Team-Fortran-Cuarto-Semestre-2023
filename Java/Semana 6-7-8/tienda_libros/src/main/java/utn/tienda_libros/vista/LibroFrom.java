449b3b9f0e96e92ea60fef321012611abd2fc45d
Morales1227 <100379618+Morales1227@users.noreply.github.com>
2023-10-26 10:34:23 -0300

    morales Gaston #338
     Avanzamos con swing y la tabla -> JTable

--- Java/Semana 6-7-8/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Java/Semana 6-7-8/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.DefaultTableModel;
 
 public class LibroFrom extends JFrame {
 
     LibroServicio libroServicio;
     private JPanel panel;
+    private JTable tablaLibros;
+    private DefaultTableModel TablaModeloLiibros;
 
     @Autowired
     public LibroFrom(LibroServicio libroServicio){
@@ -21,5 +25,35 @@ public class LibroFrom extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(900,700);
+    }
+
+    private void createUIComponents() {
+        this.TablaModeloLiibros = new DefaultTableModel(0,5);
+                String[] cabecera = {"id", "Libro", "Autor", "Precio", "Existencia"};
+                this.TablaModeloLiibros.setColumnIdentifiers(cabecera);
+                //Instanciar el objeto JTable
+        this.TablaModeloLiibros = new  JTable(TablaModeloLiibros);
+        listarLibros();
+
+    }
+    private class listarLibros(){
+        //Limpiar la tabla
+        TablaModeloLiibros.setRowCount(0);
+        //Obtener los libros de la BD
+        var libros = libroServicio.listarLibros();
+        //Iteramos cada libro
+        libros.forEach((libro) -> {
+            Object[] renglonLibro = {
+                    libro.getidLibro(),
+                    libro.getNombreLibro(),
+                    libro.getAutor(),
+                    libro.getPrecio(),
+                    libro.getExistencias(),
+            }
+    this.TablaModeloLibros.addRow(renglonLibro);
+
+        });
+
+
     }
 }
