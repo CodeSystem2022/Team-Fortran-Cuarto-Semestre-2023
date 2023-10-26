d4d9325636eb00a1d65a193a5846b4cd687f990f
Facundo Pereyra <facundo070598@gmail.com>
2023-10-26 17:32:27 -0300

    8.4 Hacemos cambios hacía un algoritmo más complejo
    8.4 Hacemos cambios hacía un algoritmo más complejo

--- Node/Semana 8/02-fundamentos_node/async/callback.js
+++ Node/Semana 8/02-fundamentos_node/async/callback.js
@@ -1,10 +1,22 @@
-function soyAsincrona(){
-    setTimeout(function(miCallback) {
-        console.log('Hola soy una funcion asincrona');
-    }, 1000);
-    
+
+function hola(miCallback){
+    setTimeout( function() {
+        console.log('Hola '+nombre);
+        miCallback();
+    }, 1000);   
 }
+
+
+function adios(nombre, otroCallback) {
+    setTimeout( function () {
+            console.log('Adios', nombre);
+            otroCallback();
+    },1000);
+}
+
 console.log('Iniciando el proceso....');
-soyAsincrona(function(){
-    console.log('Terminado el proceso...');
+hola('Carlos', function()  {
+    adios('Carlos', function() {
+        console.log('Terminado el proceso...');
+    });
 });
